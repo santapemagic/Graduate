e98b0bf9f188ecdef25e713b9a7c7962ae5ce114
santapemagic <santapemagic@gmail.com>
2013-11-01 22:28:20 +0900

    WeatherOP_v.1.5
    얼추얼추

--- WeatherOP/gen/com/example/weatherop/R.java
+++ WeatherOP/gen/com/example/weatherop/R.java
@@ -35,26 +35,31 @@ public final class R {
         public static final int snow=0x7f020003;
         public static final int sun=0x7f020004;
         public static final int thunder=0x7f020005;
-        public static final int x_base=0x7f020006;
-        public static final int y_base=0x7f020007;
+        public static final int toright=0x7f020006;
+        public static final int wood_picture=0x7f020007;
+        public static final int x_base=0x7f020008;
+        public static final int y_base=0x7f020009;
     }
     public static final class id {
-        public static final int Advice_info=0x7f090007;
-        public static final int XY_info=0x7f090008;
-        public static final int X_image=0x7f090009;
-        public static final int Y_image=0x7f09000a;
+        public static final int Advice_info=0x7f090008;
+        public static final int XY_info=0x7f090009;
+        public static final int X_image=0x7f09000a;
+        public static final int Y_image=0x7f09000b;
         public static final int action_settings=0x7f09000c;
-        public static final int input_data=0x7f090000;
+        public static final int button1=0x7f090000;
         public static final int left_day_info=0x7f090001;
         public static final int left_image=0x7f090006;
         public static final int left_weather=0x7f090005;
         public static final int right_day_info=0x7f090002;
         public static final int right_image=0x7f090004;
         public static final int right_weather=0x7f090003;
-        public static final int w_mark=0x7f09000b;
+        public static final int toright_image=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int data=0x7f030001;
+        public static final int devby=0x7f030002;
+        public static final int main=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
---
+++ WeatherOP/src/com/example/weatherop/DataActivity.java
@@ -0,0 +1,17 @@
+package com.example.weatherop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DataActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    
+	    setContentView(R.layout.data);
+	    // TODO Auto-generated method stub
+	}
+
+}
